6da58231c35818e195db7624f115f7f5ca507874
PedroLimaHS <Pedrolimahtc@live.com>
2025-04-16 17:05:24 -0300

    alterando o caminho api

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://9339-2804-954-fab7-c600-cce3-76a3-2b7f-343a.ngrok-free.app"
 });
